27c01728358fadf9c6c4fce0c0c34743b8e4c8f8
saeedalhabib2004 <S202249540@kfupm.edu.sa>
2025-11-16 17:09:09 +0300

    commit

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -169,22 +169,40 @@
 
 import mongoose from "mongoose";
 
-// establish connection
+const connectionString = "mongodb+srv://Saeed:Saeed%40SWE363@cluster0.rrj3ff9.mongodb.net/labDB?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(connectionString);
 
+const studentSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  major: String
+});
+const Student = mongoose.model("Student", studentSchema);
 
-// define schema
+async function createStudents() {
+  await Student.insertMany([
+    { name: "Ali", age: 21, major: "CS" },
+    { name: "Sara", age: 23, major: "SE" }
+  ]);
+  console.log("✅ Inserted");
+}
+createStudents
+async function readStudents() {
+  const all = await Student.find();
+  console.log(all);
+}
+readStudents();
+async function updateStudent() {
+  await Student.updateOne({ name: "Ali" }, { age: 22 });
+  console.log("✅ Updated Ali");
+}
+updateStudent()
+async function deleteStudent() {
+  await Student.deleteOne({ name: "Sara" });
+  console.log("✅ Deleted Sara");
+}
+deleteStudent()
 
-
-// create document
-
-
-// read document
-
-
-// update document
-
-
-// delete document
 
 
 
